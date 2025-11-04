0cef27cbf2421b525ed207ea0eaea76b2830bfdb
Roberto Farias <167829927+RobertoFarias86@users.noreply.github.com>
2025-11-03 21:48:47 -0300

    Create Layout.js

---
+++ src/components/Layout.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import "../styles/global.css";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="container">{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
